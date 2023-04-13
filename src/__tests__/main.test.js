bc8a4310283e5836ff251fba8ffd214678337b3a
Luke Lorenz <llorenz@middlebury.edu>
2023-04-13 09:03:45 -0400

    main_test

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -1,7 +1,7 @@
 import Home from "@/pages/index";
 import { render } from "@testing-library/react";
 // import fetchMock from "jest-fetch-mock"
-
+import mockRouter from "next-router-mock";
 // const originalFetch = global.fetch;
 // global.fetch = (url, ...params) => {
 //   if (typeof url === "string" && url.startsWith("/")) {
@@ -12,6 +12,7 @@ import { render } from "@testing-library/react";
 
 describe("End-to-end testing", () => {
   test("Render index.js component", () => {
+    mockRouter.push("/");
     render(<Home />);
   });
 });
