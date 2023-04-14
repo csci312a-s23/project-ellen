0b8c99f9266ec30252fb3d20dbdf86480fd6f3e8
Luke Lorenz <llorenz@middlebury.edu>
2023-04-14 15:52:42 -0400

    alexis comment

--- public/components/postsList.test.js
+++ public/components/postsList.test.js
@@ -1,36 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import PostList from "./postsList";
 import fetchMock from "fetch-mock-jest";
+import { PostSeedData } from "../../data/SeedData.json";
 
 describe("postList: postList tests", () => {
   beforeEach(() => {
-    fetchMock.get("begin:/api/posts", () => {
-      return [
-        {
-          title: "title1",
-          posterID: 1,
-          content: "content1",
-          category: "school",
-          votes: 100,
-          created_at: new Date(),
-        },
-        {
-          title: "title2",
-          posterID: 2,
-          content: "content2",
-          category: "food",
-          votes: 0,
-          created_at: new Date(),
-        },
-        {
-          title: "title3",
-          posterID: 2,
-          content: "content3",
-          category: "food",
-          votes: 20,
-          created_at: new Date(),
-        },
-      ];
+    fetchMock.get("begin:", () => {
+      return PostSeedData;
     });
   });
 
