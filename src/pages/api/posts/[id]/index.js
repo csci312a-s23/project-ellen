c79a9704632a27b53c48a8b25d34ae19a4b763fc
Rohan Datar <37545071+rohan-datar@users.noreply.github.com>
2023-04-14 16:29:42 -0400

    fix for error handling

--- src/pages/api/posts/[id]/index.js
+++ src/pages/api/posts/[id]/index.js
@@ -1,14 +1,17 @@
 import nc from "next-connect";
 import Posts from "../../../../../models/Posts.js";
-// import { knex } from "../../../../knex/knex.js";
+import { onError } from "../../../../../lib/middleware.js";
 // interaciton with post based on id
 
-const handler = nc()
+const handler = nc({ onError })
   .get(async (req, res) => {
     const { id } = req.query;
 
     if (!!id) {
-      const post = await Posts.query().findById(parseInt(id)).first();
+      const post = await Posts.query()
+        .findById(parseInt(id))
+        .first()
+        .throwIfNotFound();
 
       res.status(200).json(post);
     }
