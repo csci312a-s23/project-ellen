c0bf45f59e11977d48cfb43aff0b6d85048767c7
Rohan Datar <37545071+rohan-datar@users.noreply.github.com>
2023-04-18 09:17:46 -0400

    added test for delete

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -15,17 +15,17 @@ const fs = require("fs");
 const contents = fs.readFileSync("./data/SeedData.json");
 const data = JSON.parse(contents);
 
-describe("API Post tests", () => {
-  beforeAll(() => {
-    // Ensure test database is initialized before an tests
-    return knex.migrate.rollback().then(() => knex.migrate.latest());
-  });
-
-  beforeAll(() => {
-    // Reset contents of the test database
-    return knex.seed.run();
-  });
+describe("API tests", () => {
   describe("Posts API tests", () => {
+    beforeAll(() => {
+      // Ensure test database is initialized before an tests
+      return knex.migrate.rollback().then(() => knex.migrate.latest());
+    });
+
+    beforeAll(() => {
+      // Reset contents of the test database
+      return knex.seed.run();
+    });
     test("GET /api/posts should return all posts", async () => {
       await testApiHandler({
         rejectOnHandlerError: true, // Make sure to catch any errors
@@ -152,9 +152,36 @@ describe("API Post tests", () => {
         },
       });
     });
+
+    test("DELETE /api/posts/[id] should delete post", async () => {
+      await testApiHandler({
+        rejectOnHandlerError: true, // Make sure to catch any errors
+        handler: individualPost_endpoint, // NextJS API function to test
+        url: "/api/posts/1",
+        paramsPatcher: (params) => (params.id = 1), // Testing dynamic routes requires patcher
+        test: async ({ fetch }) => {
+          // Test endpoint with mock fetch
+          const res = await fetch({
+            method: "DELETE",
+          });
+
+          const response = await res.json();
+          expect(response.message).toBe("Post deleted");
+        },
+      });
+    });
   });
 
   describe("User API tests", () => {
+    beforeAll(() => {
+      // Ensure test database is initialized before an tests
+      return knex.migrate.rollback().then(() => knex.migrate.latest());
+    });
+
+    beforeAll(() => {
+      // Reset contents of the test database
+      return knex.seed.run();
+    });
     test("GET /api/users should return all users", async () => {
       await testApiHandler({
         rejectOnHandlerError: true, // Make sure to catch any errors
@@ -319,7 +346,6 @@ describe("API Comment tests", () => {
         });
 
         const response = await res.json();
-        console.log(response);
 
         expect(response).toHaveProperty("commenterID", "2");
         expect(response).toHaveProperty("postID", 1);
