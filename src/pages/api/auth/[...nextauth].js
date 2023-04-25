2ebb49ee3e56f8e6a243663fb52429db174de5d9
AlexisBallo2 <alexispballo@gmail.com>
2023-04-25 08:35:46 -0400

    cleanup

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,7 +5,6 @@ import { Scraper } from "directory.js";
 
 export const authOptions = {
   // Configure one or more authentication providers
-  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
