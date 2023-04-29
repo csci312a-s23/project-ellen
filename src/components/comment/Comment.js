188c1085c448ba92f15d213d88efb194d50e5d91
AlexisBallo2 <alexispballo@gmail.com>
2023-04-29 12:38:50 -0400

    pass tests

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -20,31 +20,35 @@ export default function Comment({ data, vote }) {
         marginBottom: 2,
       }}
     >
-      {/* <div data-testid="comment"> */}
-      <Box sx={{ width: "70%", paddingLeft: 2 }}>
-        <div>{data.content}</div>
-      </Box>
+      <div data-testid="comment">
+        <Box sx={{ width: "70%", paddingLeft: 2 }}>
+          <div>{data.content}</div>
+        </Box>
 
-      <Box
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <div
-          style={{ cursor: "pointer" }}
-          onClick={() => vote("upvote", data.id)}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          {" "}
-          <KeyboardArrowUpIcon />{" "}
-        </div>
-        <p>likes: {data.likes}</p>
-        <div
-          style={{ cursor: "pointer" }}
-          onClick={() => vote("downvote", data.id)}
-        >
-          {" "}
-          <KeyboardArrowDownIcon />{" "}
-        </div>
-      </Box>
-      {/* </div> */}
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => vote("upvote", data.id)}
+          >
+            {" "}
+            <KeyboardArrowUpIcon />{" "}
+          </div>
+          <p>likes: {data.likes}</p>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => vote("downvote", data.id)}
+          >
+            {" "}
+            <KeyboardArrowDownIcon />{" "}
+          </div>
+        </Box>
+      </div>
     </Box>
   );
 }
