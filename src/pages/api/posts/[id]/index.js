0e4241cde8c97c3b161befd0f5ad49f2c06040a6
Rohan Datar <37545071+rohan-datar@users.noreply.github.com>
2023-05-03 16:50:32 -0400

    Update index.js

--- src/pages/api/posts/[id]/index.js
+++ src/pages/api/posts/[id]/index.js
@@ -25,14 +25,11 @@ const handler = nc({ onError })
       let myVote = 0;
 
       if (session) {
-        const user = await Users.query()
-          .findById(session.user.id)
-          .throwIfNotFound();
 
         const myVoteRow = await Votes.query()
           .where("postID", parseInt(id))
           .where("typeOf", "post")
-          .where("voterID", user.id);
+          .where("voterID", session.user.id);
 
         if (myVoteRow.length !== 0) {
           myVote = myVoteRow[0].value;
