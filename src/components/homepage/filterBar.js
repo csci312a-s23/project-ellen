e55683cf2c7ef7b91b751aff3e461475441c86da
AlexisBallo2 <alexispballo@gmail.com>
2023-05-07 15:33:45 -0400

    only show filters on index

--- src/components/homepage/filterBar.js
+++ src/components/homepage/filterBar.js
@@ -15,9 +15,10 @@
 
 import Button from "@mui/material/Button";
 import { Menu, MenuItem } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useRouter } from "next/router";
 
 const buttonStyle = {
   variant: "outlined",
@@ -27,6 +28,13 @@ const buttonStyle = {
 function FilterBar({ currentSortFilter, setCurrentSortFilter }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+  const router = useRouter();
+  const [page, setPage] = useState();
+
+  useEffect(() => {
+    setPage(router.asPath);
+    // console.log("router", router)
+  }, [router]);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -61,63 +69,65 @@ function FilterBar({ currentSortFilter, setCurrentSortFilter }) {
           paddingBottom: "200px",
         }}
       >
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <Button
-            {...buttonStyle}
-            onClick={() => setCurrentSortFilter("new")}
-            disabled={currentSortFilter === "new"}
-          >
-            New
-          </Button>
-          <Button
-            {...buttonStyle}
-            onClick={() => setCurrentSortFilter("hot")}
-            disabled={currentSortFilter === "hot"}
-          >
-            Hot
-          </Button>
+        {page === "/" && (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <Button
+              {...buttonStyle}
+              onClick={() => setCurrentSortFilter("new")}
+              disabled={currentSortFilter === "new"}
+            >
+              New
+            </Button>
+            <Button
+              {...buttonStyle}
+              onClick={() => setCurrentSortFilter("hot")}
+              disabled={currentSortFilter === "hot"}
+            >
+              Hot
+            </Button>
 
-          <Button
-            {...buttonStyle}
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            {currentSortFilter === "new" || currentSortFilter === "hot" ? (
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  flexWrap: "wrap",
-                }}
-              >
-                <span>Category</span>
-                <KeyboardArrowDownIcon />
-              </div>
-            ) : (
-              currentSortFilter
-            )}
-          </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={changeCategory}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={changeCategory}>Academics</MenuItem>
-            <MenuItem onClick={changeCategory}>Athletics</MenuItem>
-            <MenuItem onClick={changeCategory}>Social</MenuItem>
-            <MenuItem onClick={changeCategory}>Professors</MenuItem>
-            <MenuItem onClick={changeCategory}>Housing</MenuItem>
-            <MenuItem onClick={changeCategory}>Dining</MenuItem>
-            <MenuItem onClick={changeCategory}>Other</MenuItem>
-          </Menu>
-        </div>
+            <Button
+              {...buttonStyle}
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              {currentSortFilter === "new" || currentSortFilter === "hot" ? (
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <span>Category</span>
+                  <KeyboardArrowDownIcon />
+                </div>
+              ) : (
+                currentSortFilter
+              )}
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={changeCategory}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={changeCategory}>Academics</MenuItem>
+              <MenuItem onClick={changeCategory}>Athletics</MenuItem>
+              <MenuItem onClick={changeCategory}>Social</MenuItem>
+              <MenuItem onClick={changeCategory}>Professors</MenuItem>
+              <MenuItem onClick={changeCategory}>Housing</MenuItem>
+              <MenuItem onClick={changeCategory}>Dining</MenuItem>
+              <MenuItem onClick={changeCategory}>Other</MenuItem>
+            </Menu>
+          </div>
+        )}
       </div>
     </div>
   );
