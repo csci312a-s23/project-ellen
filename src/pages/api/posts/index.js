860074f0435e9cad75835f6da1e40682259f84f4
Rohan Datar <37545071+rohan-datar@users.noreply.github.com>
2023-05-09 10:38:40 -0400

    removing duplicate functionality

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -2,8 +2,6 @@ import nc from "next-connect";
 import Post from "../../../../models/Posts.js";
 import { onError } from "../../../lib/middleware.js";
 import { authenticated } from "../../../lib/middleware.js";
-import { authOptions } from "../auth/[...nextauth].js";
-import { getServerSession } from "next-auth/next";
 
 // function to handle returning all posts
 const handler = nc({ onError })
@@ -19,7 +17,7 @@ const handler = nc({ onError })
     res.status(200).json(posts);
   })
   .post(authenticated, async (req, res) => {
-    const session = await getServerSession(req, res, authOptions);
+    // const session = await getServerSession(req, res, authOptions);
     const { body } = req;
 
     if (!body) {
@@ -28,7 +26,7 @@ const handler = nc({ onError })
     }
 
     const newPost = await Post.query().insertAndFetch({
-      posterID: session.user.id,
+      posterID: req.user.id,
       title: body?.title,
       content: body?.content,
       category: body?.category,
