069e714908ec66cabbb85efb70d7073382856b69
Matt Dailey <mdailey@middlebury.edu>
2023-05-11 11:30:45 -0400

    cleaned up nav bar and added drop down menu

---
+++ src/components/ProfileIcon.js
@@ -0,0 +1,72 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import PortraitIcon from "@mui/icons-material/Portrait";
+import React, { useState } from "react";
+import { Menu, MenuItem } from "@mui/material";
+import Link from "next/link";
+
+export default function ProfileIcon() {
+  const { data: session } = useSession();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  if (!session) {
+    return (
+      <div>
+        <PortraitIcon onClick={handleMenuClick} />
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem
+            sx={{ backgroundColor: "grey" }}
+            variant="contained"
+            onClick={() => signIn("google")}
+          >
+            Sign in
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <PortraitIcon onClick={handleMenuClick} />
+
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose}>
+            {session && (
+              <Link
+                href={`/profile/${session.user.name}`}
+                style={{ textDecoration: "none" }}
+              >
+                Profile
+              </Link>
+            )}
+          </MenuItem>
+
+          <MenuItem
+            sx={{ backgroundColor: "grey" }}
+            variant="contained"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  }
+}
