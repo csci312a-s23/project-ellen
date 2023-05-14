39d6c27cde953004e54e58117221be0359d2bed5
Luke Lorenz <llorenz@middlebury.edu>
2023-05-14 16:01:56 -0400

    merge main and clean up comment container

--- src/components/comment/CommentsContainer.js
+++ src/components/comment/CommentsContainer.js
@@ -1,41 +1,20 @@
-// import Link from "next/link";
 import Comment from "./Comment";
 
-export default function CommentsContainer({
-  comments,
-  vote,
-  whereis,
-  deleteComment,
-}) {
+export default function CommentsContainer({ comments, vote, deleteComment }) {
   let commentBlocks = <div> NO comments </div>;
 
-  if (comments.length > 0) {
-    commentBlocks = comments.map((comment) => {
-      return (
-        <div key={comment.id}>
-          {(() => {
-            if (whereis === "profile") {
-              return (
-                <Comment
-                  data={comment}
-                  vote={vote}
-                  deleteComment={deleteComment}
-                />
-              );
-            } else {
-              return (
-                <Comment
-                  data={comment}
-                  vote={vote}
-                  deleteComment={deleteComment}
-                />
-              );
-            }
-          })()}
-        </div>
-      );
-    });
-  }
+  commentBlocks =
+    comments.length > 0 ? (
+      comments.map((comment) => {
+        return (
+          <div key={comment.id}>
+            <Comment data={comment} vote={vote} deleteComment={deleteComment} />
+          </div>
+        );
+      })
+    ) : (
+      <div> NO comments </div>
+    );
 
   return <div>{commentBlocks}</div>;
 }
