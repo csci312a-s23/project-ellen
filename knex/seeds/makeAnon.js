9af8f21393f977bc02537d871b7bf6059a2e86ca
Liam Patty <liamh.patty@gmail.com>
2023-05-17 21:30:00 -0400

    fixes

--- knex/seeds/makeAnon.js
+++ knex/seeds/makeAnon.js
@@ -1,6 +1,6 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("users").del();
+  //await knex("users").del();
   await knex("users").insert({
     id: "51e2c13b-dfd5-4ed4-8805-f03357a3083b",
     username: "anonymous",
